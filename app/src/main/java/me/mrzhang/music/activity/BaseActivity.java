bb24d862345fb48f0e3262215ede245b8fd20dca
2586249084 <2586249084@qq.com>
2018-08-21 10:21:39 +0800

    master

---
+++ app/src/main/java/me/mrzhang/music/activity/BaseActivity.java
@@ -0,0 +1,30 @@
+package me.mrzhang.music.activity;
+
+import android.graphics.Color;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.WindowManager;
+
+public abstract class BaseActivity extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setSystemBarTransparent();
+    }
+
+    private void setSystemBarTransparent(){
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            // LOLLIPOP解决方案
+            getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
+            getWindow().addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            getWindow().setStatusBarColor(Color.TRANSPARENT);
+        } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            // KITKAT解决方案
+            getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+        }
+    }
+}
---
+++ app/src/main/java/me/mrzhang/music/utils/binding/ViewBinder.java
@@ -0,0 +1,36 @@
+package me.mrzhang.music.utils.binding;
+
+import android.app.Activity;
+import android.util.Log;
+import android.view.View;
+import java.lang.reflect.Field;
+
+public class ViewBinder {
+
+    public static void bind(Activity activity){
+        bind(activity, activity.getWindow().getDecorView());
+    }
+
+    private static void bind(Object target, View source){
+        Field[] fields = target.getClass().getDeclaredFields();
+        if (fields != null && fields.length > 0) {
+            for (Field field : fields) {
+                try {
+                    field.setAccessible(true);
+                    if (field.get(target) != null) {
+                        continue;
+                    }
+
+                    Bind bind = field.getAnnotation(Bind.class);
+                    if (bind != null) {
+                        int viewId = bind.value();
+                        field.set(target, source.findViewById(viewId));
+                    }
+                } catch (Exception error) {
+                    Log.e("Binding Error:", error.getLocalizedMessage());
+                    error.printStackTrace();
+                }
+            }
+        }
+    }
+}
